66cd0ba292ba7f2f8fc8e3c90295006f73364cb8
Carlos Claro <carlosclaro79@gmail.com>
2021-10-15 13:13:47 -0300

    implementaçao de mapa na ficha do imovel, com react-google-maps/api

---
+++ src/components/Imoveis/mapas.js
@@ -0,0 +1,4 @@
+https://www.npmjs.com/package/@react-google-maps/api
+https://www.creative-tim.com/learning-lab/nextjs/react-google-maps/material-dashboard
+https://tomchentw.github.io/react-google-maps/#introduction
+key: AIzaSyDtEmaLBS8ts1Ur_CUwLTOhD7zL0noNIHQ
