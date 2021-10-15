c1f1b36808f7e2823aaf5778bcb031cf6d4a33cd
Carlos Claro <carlosclaro79@gmail.com>
2021-10-15 14:12:51 -0300

    implementa @react-google-maps/api

--- src/components/Imoveis/mapas.js
+++ src/components/Imoveis/mapas.js
@@ -1,4 +1,3 @@
 https://www.npmjs.com/package/@react-google-maps/api
 https://www.creative-tim.com/learning-lab/nextjs/react-google-maps/material-dashboard
 https://tomchentw.github.io/react-google-maps/#introduction
-key: AIzaSyDtEmaLBS8ts1Ur_CUwLTOhD7zL0noNIHQ
